8b101b5b9ed0eff5c8198570b42db294751c3717
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2013-11-09 14:46:46 -0500

    more style changes

--- src/documents/scripts/app.js
+++ src/documents/scripts/app.js
@@ -5,14 +5,14 @@
         .on('click', '.icon-list', function(){
             $body.toggleClass('menu-open');
         })
-        .on('mouseover', '.agenda-event-actions', function(){
-            var iframe = $(this).parents('.agenda-event').find('iframe'),
-                src = iframe.attr('src');
+        // .on('mouseover', '.agenda-event-actions', function(){
+        //     var iframe = $(this).parents('.agenda-event').find('iframe'),
+        //         src = iframe.attr('src');
 
-            if(!src){
-                iframe.attr('src', iframe.data('src'));
-            }
-        })
+        //     if(!src){
+        //         iframe.attr('src', iframe.data('src'));
+        //     }
+        // })
         .on('click', 'button[title="view map"]', function(){
             $(this).parents('.agenda-event').addClass('is-exposed');
         })
